a43a8887cc5d05662f5c5c1af852be3207152f4d
kjxbyz <47768002+kjxbyz@users.noreply.github.com>
2023-08-11 20:40:20 +0800

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import "./globals.css";
+import cx from "classnames";
+import { sfPro, inter } from "./fonts";
+import Nav from "@/components/layout/nav";
+import Footer from "@/components/layout/footer";
+import { Suspense } from "react";
+
+export const metadata = {
+  title: "kjxbyz",
+  description: "科技小白英仔.",
+  metadataBase: new URL("https://kjxbyz.com"),
+  themeColor: "#FFF",
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={cx(sfPro.variable, inter.variable)}>
+        <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
+        <Suspense fallback="...">
+          {/* @ts-ignore */}
+          <Nav />
+        </Suspense>
+        <main
+          id="main"
+          className="flex min-h-screen w-full flex-col items-center justify-center py-32"
+        >
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
