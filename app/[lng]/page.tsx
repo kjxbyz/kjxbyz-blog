d25f12fefbea3ba0899807f96c5943967153d09b
kjxbyz <47768002+kjxbyz@users.noreply.github.com>
2023-09-23 13:51:07 +0800

    fix: home page.

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -74,7 +74,7 @@ export default function Home({
           </a>
         </div>
       </div>
-      <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">
+      <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 sm:grid-cols-2 lg:grid-cols-3 xl:px-0">
         {features.map(({ title, description, demo, url, large }) => (
           <DynamicCard
             key={title}
