af5fa75d8eb047c8d93f5c4b1ab66bc147e68c85
kjxbyz <47768002+kjxbyz@users.noreply.github.com>
2023-10-02 16:41:44 +0800

    feat: add Web Analytics

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -6,6 +6,7 @@ import { Metadata } from "next";
 import dynamic from "next/dynamic";
 import NextTopLoader from "nextjs-toploader";
 import { BiArrowToTop } from "react-icons/bi";
+import { Analytics } from "@vercel/analytics/react";
 import ScrollToTop from "@/components/layout/scroll-to-top";
 import Footer from "@/components/layout/footer";
 import { languages } from "@/i18n/settings";
@@ -56,6 +57,7 @@ export default async function RootLayout({
             className="flex min-h-screen w-full flex-col items-center justify-center py-32"
           >
             {children}
+            <Analytics />
           </main>
           <Footer lng={params.lng} />
         </Providers>
