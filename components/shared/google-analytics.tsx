6ea7746127244b5fc733efbec66f7ceeaec84184
kjxbyz <kjxbyz@163.com>
2023-10-08 19:46:45 +0800

    fix: remove `console.log`

--- components/shared/google-analytics.tsx
+++ components/shared/google-analytics.tsx
@@ -3,8 +3,8 @@ import Script from "next/script";
 
 const GoogleAnalytics = () => {
   const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ID;
-  //You can show in the console the GA_TRACKING_ID to confirm
-  console.log(GA_TRACKING_ID);
+  // You can show in the console the GA_TRACKING_ID to confirm
+  // console.log(GA_TRACKING_ID);
 
   return (
     <>
