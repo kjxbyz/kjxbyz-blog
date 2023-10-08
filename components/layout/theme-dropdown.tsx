2f3dc660cc2b85a628d8f4bfff51065b383491c6
kjxbyz <kjxbyz888@outlook.com>
2023-10-08 19:45:39 +0800

    refactor: i18n & theme

--- components/layout/theme-dropdown.tsx
+++ components/layout/theme-dropdown.tsx
@@ -10,7 +10,7 @@ import { LngProps } from "@/i18next-lng";
 import { themes, icons, Theme, ThemeMode } from "@/theme";
 
 export default function ThemeDropdown(props: LngProps) {
-  const { t } = useTranslation(props.lng);
+  const { t } = useTranslation(props.lng, "header");
   const { theme, setTheme } = useTheme();
   const [openPopover, setOpenPopover] = useState(false);
 
@@ -23,19 +23,19 @@ export default function ThemeDropdown(props: LngProps) {
       <Popover
         content={
           <div className="w-full min-w-[14rem] rounded-md bg-white p-2 dark:bg-black">
-            {themes.map((t: Theme) => {
+            {themes.map((t1: Theme) => {
               return (
                 <button
-                  key={t.mode}
-                  onClick={() => setTheme(t.mode)}
+                  key={t1.mode}
+                  onClick={() => setTheme(t1.mode)}
                   className={`relative flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 ${
-                    theme === t.mode
+                    theme === t1.mode
                       ? "cursor-not-allowed bg-gray-100 dark:bg-gray-700"
                       : ""
                   }`}
                 >
-                  <t.icon className="mr-2" />
-                  <p className="text-sm">{t.name}</p>
+                  <t1.icon className="mr-2" />
+                  <p className="text-sm">{t(`menus.${t1.mode}`)}</p>
                 </button>
               );
             })}
