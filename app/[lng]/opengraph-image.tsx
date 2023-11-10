53e52447be1eef56a76f6f8dae4af181e7689b14
kjxbyz <47768002+kjxbyz@users.noreply.github.com>
2023-11-10 19:00:27 +0800

    fix: replace all occurrences of `Precedent`

--- app/[lng]/opengraph-image.tsx
+++ app/[lng]/opengraph-image.tsx
@@ -28,7 +28,7 @@ export default async function OG() {
       >
         <img
           src={`${basePath}/logo.jpg`}
-          alt="Precedent Logo"
+          alt="kjxbyz Logo"
           tw="w-20 h-20 mb-4 opacity-95"
         />
         <h1
@@ -43,7 +43,7 @@ export default async function OG() {
             letterSpacing: "-0.02em",
           }}
         >
-          Precedent
+          kjxbyz
         </h1>
       </div>
     ),
