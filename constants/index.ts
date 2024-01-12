7c9bd8f286f3efb24e5c2fbd579c21076206e11a
kjxbyz <47768002+kjxbyz@users.noreply.github.com>
2024-01-12 22:05:09 +0800

    feat: add rss

--- constants/index.ts
+++ constants/index.ts
@@ -2,3 +2,7 @@ export const cacheLngKey: string = "__blog_lng__";
 export const cacheThemeKey: string = "__blog_theme__";
 export const basePath =
   process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? "/portal" : "";
+export const host =
+  process.env.NODE_ENV === "production"
+    ? `https://kjxbyz.com${basePath}`
+    : `http://localhost:3000${basePath}`;
