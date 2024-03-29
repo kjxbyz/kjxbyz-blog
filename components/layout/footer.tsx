3a41990ffdc9a78f47607e890b57ad700422dd20
kjxbyz <47768002+kjxbyz@users.noreply.github.com>
2024-03-29 08:56:38 +0800

    fix: update repo links

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -18,7 +18,7 @@ export default function Footer(props: LngProps) {
       {/*  {t("footer")}{" "}*/}
       {/*  <a*/}
       {/*    className="font-medium text-gray-800 underline transition-colors dark:text-white/90"*/}
-      {/*    href="https://github.com/kjxbyz/kjxbyz.github.io"*/}
+      {/*    href="https://github.com/kjxbyz/kjxbyz-blog"*/}
       {/*    target="_blank"*/}
       {/*    rel="noopener noreferrer"*/}
       {/*  >*/}
@@ -55,7 +55,7 @@ export default function Footer(props: LngProps) {
         {process.env.VERCEL_GIT_COMMIT_SHA && (
           <p className="flex items-center justify-center">
             <a
-              href={`https://github.com/kjxbyz/kjxbyz.github.io/commit/${process.env.VERCEL_GIT_COMMIT_SHA}`}
+              href={`https://github.com/kjxbyz/kjxbyz-blog/commit/${process.env.VERCEL_GIT_COMMIT_SHA}`}
               target="_blank"
               className="hover:underline"
               rel="noreferrer"
